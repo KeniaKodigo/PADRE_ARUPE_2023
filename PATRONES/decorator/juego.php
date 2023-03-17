e3abdd1d8c492a0b6dffdd89481449f37d8ffd91
KenyaPaiz <paizkenia5@gmail.com>
2023-03-17 17:30:20 -0600

    patrones estructurales y comportamiento

--- PATRONES/decorator/juego.php
+++ PATRONES/decorator/juego.php
@@ -6,3 +6,74 @@
  * personajes: guerrero, batman
  */
 
+interface personajes{
+    public function herramientas();
+}
+
+/** Clases concretas */
+class Guerrero implements personajes{
+    public function herramientas()
+    {
+        echo "El guerreo cuenta con estas herramientas: ";
+    }
+}
+
+class Batman implements personajes{
+    public function herramientas()
+    {
+        echo "Batman cuenta con estas herramientas: ";
+    }
+}
+
+class Iroman implements personajes{
+    public function herramientas()
+    {
+        echo "Iroman cuenta con estas herramientas: ";
+    }
+}
+
+/** Clase decoradora */
+class decoratorPersonaje implements personajes{
+    protected $personaje;
+    public function __construct(personajes $personaje)
+    {
+        $this->personaje = $personaje;
+    }
+
+    public function herramientas()
+    {
+        return $this->personaje->herramientas();
+    }
+}
+
+/** herramientas extras como decoraciones*/
+class Espada extends decoratorPersonaje{
+    public function herramientas(){
+        return parent::herramientas() . "lleva una espada ";
+    }
+}
+
+class Arma extends decoratorPersonaje{
+    public function herramientas(){
+        return parent::herramientas() . " lleva una arma";
+    }
+}
+
+class Bomba extends decoratorPersonaje{
+    public function herramientas(){
+        return parent::herramientas() . " lleva una bomba";
+    }
+}
+
+
+/** Metodo cliente */
+function getPersonajes(personajes $personaje){
+    return $personaje->herramientas();
+}
+
+/** Instanciando */
+$guerrero = new Guerrero;
+$guerrero = new Espada($guerrero);
+$guerrero = new Arma($guerrero);
+
+echo getPersonajes($guerrero);
--- PATRONES/strategy/mensajes.php
+++ PATRONES/strategy/mensajes.php
@@ -5,4 +5,52 @@
  * para tener por separado los tipos de salida necesitamos utilizar el patron strategy. 
  */
 
+/** Interfaz strategy */
+interface tipoSalida{
+    public function formato();
+}
+
+/** creanado las clases que dependan de la interfaz */
+class Consola implements tipoSalida{
+    public function formato()
+    {
+        echo "El mensaje es por via consola";
+    }
+}
+
+class JSON implements tipoSalida{
+    public function formato()
+    {
+        echo "El mensaje es por via JSON";
+    }
+}
+
+class Texto implements tipoSalida{
+    public function formato()
+    {
+        echo "El mensaje es por via texto";
+    }
+}
+
+/** Clase Concreta para recibir las estrategias */
+class ImprimirFormato{
+    private tipoSalida $formato;
+
+    public function setFormato(tipoSalida $salida){
+        $this->formato = $salida;
+    }
+
+    public function formato(){
+        echo $this->formato->formato();
+    }
+}
+
+$formato = new ImprimirFormato();
+
+$formato->setFormato(new JSON());
+echo $formato->formato();
+echo "\n";
+$formato->setFormato(new Consola());
+echo $formato->formato();
+
 ?>
